c4a83c2d74711ac9ae35e7bbfa2fef1a0e8be3ee
Aubrey Whitford <Aubreyschifano@gmail.com>
2023-04-23 11:14:37 -0400

    adds functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+  let cards = [];
+
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    cards.push(message);
+  }
+  return cards;
+}
+
+names(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(number) {
+  let i = number;
+while (i >= 0) {
+  console.log(i--);
+  }
+}
+
+countDown(10)
