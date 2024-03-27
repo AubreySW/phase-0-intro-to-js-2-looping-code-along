59c89a684dd8b0c787887e8f70fdb4070834f73c
Aubrey Whitford <Aubreya.whitford@gmail.com>
2024-03-27 16:30:29 -0400

    completes lab

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
+/*const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow`);
+        debugger
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
 
 function writeCards(names, event) {
-  let cards = [];
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
 
-  for (let i = 0; i < names.length; i++) {
-    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
-    cards.push(message);
-  }
-  return cards;
+    return messages;
 }
 
-names(["Charlie", "Samip", "Ali"], "birthday");
-
-function countDown(number) {
-  let i = number;
-while (i >= 0) {
-  console.log(i--);
-  }
+function countDown(number){
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
 }
-
-countDown(10)
